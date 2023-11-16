b33e5e4f8e09c04bf0f776f2e728ce988132ee28
consultant <mamarezeleke2@gmail.com>
2023-11-16 08:43:56 -0500

    First commit

---
+++ src/test/java/com/naming/service/ServiceNamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naming.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
